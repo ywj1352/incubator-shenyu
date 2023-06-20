eb60d349132c09f5f0b1590d817c9fe416ca79a1
ywj1352 <18856309550@163.com>
2023-06-20 22:20:10 +0800

    change integratedtest about upload

--- shenyu-integrated-test/shenyu-integrated-test-upload-plugin/shenyu-integrated-test-upload-plugin-case/src/test/java/org/apache/shenyu/integrated/test/upload/plugin/AdminPluginUploadTest.java
+++ shenyu-integrated-test/shenyu-integrated-test-upload-plugin/shenyu-integrated-test-upload-plugin-case/src/test/java/org/apache/shenyu/integrated/test/upload/plugin/AdminPluginUploadTest.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shenyu.integrated.test.admin;
+package org.apache.shenyu.integrated.test.upload.plugin;
 
 import org.apache.shenyu.common.dto.ConditionData;
 import org.apache.shenyu.common.dto.SelectorData;
